4d243b39ae04834d1a90d893442527974f78232c
Sheng Wenzeng <45939754+SWZDL@users.noreply.github.com>
2021-07-30 15:59:22 +0800

    完善我的-账户设置页面
    Signed-off-by: Sheng Wenzeng <45939754+SWZDL@users.noreply.github.com>

--- miniprogram/pages/person-page/account/account.js
+++ miniprogram/pages/person-page/account/account.js
@@ -1,4 +1,7 @@
 // pages/person-page/account/account.js
+import {promisic} from "../../../miniprogram_npm/lin-ui/utils/util"
+import Toast from '../../../miniprogram_npm/@vant/weapp/toast/toast'
+
 const app = getApp()
 Page({
 
@@ -8,59 +11,145 @@ Page({
   data: {
     CustomBar: app.globalData.CustomBar,
     StatusBar: app.globalData.StatusBar,
-    logged: app.globalData.logged,
-    userInfo: app.globalData.userInfo,
-    hasRefresherTriggered: false
+    logged: false,
+    userInfo: null,
+    showChangeGender: false,
+    actions: [
+      {name: '男', color: '#000000'},
+      {name: '女', color: '#000000'}
+    ]
+  },
+  changeGenderShowStatus() {
+    this.setData({
+      showChangeGender: true
+    })
+  },
+  closeChangeGender() {
+    this.setData({
+      showChangeGender: false
+    })
+  },
+  changeGender(res) {
+    console.log(res.detail.name)
+    if (res.detail.name.search("当前.*") === -1) {
+      Toast('修改成功')
+    }
+  },
+  copyOpenID(res) {
+    console.log(res)
+    promisic(wx.setClipboardData)({data: 'data'})
+  },
+  goToVip() {
+    wx.navigateTo({
+      url: '/pages/person-page/account/vip/vip',
+      success: res => {
+        // 通过eventChannel向被打开页面传送数据
+        //   res.eventChannel.emit('result', {
+        //     data: this.data.detectResult.result
+        //   })
+        //   res.eventChannel.emit('img', {
+        //     data: this.data.img
+        //   })
+        // }
+      }
+    })
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: async function (options) {
+    //如果全局有信息,直接同步过来
+    if (app.globalData.logged && app.globalData.userInfo) {
+      console.log("全局有信息,同步过来")
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        logged: app.globalData.logged
+      })
+    } else {
+      // console.log("从本地缓存读取信息")
+      const res = wx.getStorageInfoSync()
+      console.log("查看本地是否存在userInfo的缓存", res.keys.indexOf('userInfo') === 0)
+      if (res.keys.indexOf('userInfo') !== -1) {
+        const userInfo = await promisic(wx.getStorage)({
+          key: 'userInfo'
+        })
+        // console.log("获取到userInfo", userInfo)
+        this.setData({
+          logged: true,
+          userInfo: userInfo.data
+        })
+      }
+    }
+    /*
+    * 展示性别颜色
+    */
+    if (this.data.userInfo != null) {
+      console.log(this.data.userInfo)
+      let actions = this.data.actions
+      if (this.data.userInfo._gender === 1) {
+        actions[0].name = '当前：男'
+        actions[0].color = '#06C05F'
+      } else {
+        actions[1].name = '当前：女'
+        actions[1].color = '#06C05F'
+      }
+      this.setData({
+        actions
+      })
+    }
+  }
 
-  },
+  ,
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
 
-  },
+  }
+  ,
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
 
-  },
+  }
+  ,
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
 
-  },
+  }
+  ,
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
 
-  },
+  }
+  ,
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
 
-  },
+  }
+  ,
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
 
-  },
+  }
+  ,
 
   /**
    * 用户点击右上角分享
