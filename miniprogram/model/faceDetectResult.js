99abeef695a537f7fabee21c361d84fa125e51b4
Sheng Wenzeng <45939754+SWZDL@users.noreply.github.com>
2021-09-05 17:56:14 +0800

    :sparkles:添加了识别结果实体类

---
+++ miniprogram/model/faceDetectResult.js
@@ -0,0 +1,178 @@
+class FaceDetectResult {
+  #_id
+  #_openid
+  #_age
+  #_beauty
+  #_emotion
+  #_eye
+  #_eyebrow
+  #_gender
+  #_hair
+  #_hat
+  #_headPose
+  #_mask
+  #_moustache
+  #_mouth
+  #_nose
+  #_shape
+  #_skin
+  #_smile
+  #_requestId
+
+  constructor() {
+  }
+
+  get id() {
+    return this.#_id
+  }
+
+  set id(value) {
+    this.#_id = value
+  }
+
+  get openid() {
+    return this.#_openid
+  }
+
+  set openid(value) {
+    this.#_openid = value
+  }
+
+  get age() {
+    return this.#_age
+  }
+
+  set age(value) {
+    this.#_age = value
+  }
+
+  get beauty() {
+    return this.#_beauty
+  }
+
+  set beauty(value) {
+    this.#_beauty = value
+  }
+
+  get emotion() {
+    return this.#_emotion
+  }
+
+  set emotion(value) {
+    this.#_emotion = value
+  }
+
+  get eye() {
+    return this.#_eye
+  }
+
+  set eye(value) {
+    this.#_eye = value
+  }
+
+  get eyebrow() {
+    return this.#_eyebrow
+  }
+
+  set eyebrow(value) {
+    this.#_eyebrow = value
+  }
+
+  get gender() {
+    return this.#_gender
+  }
+
+  set gender(value) {
+    this.#_gender = value
+  }
+
+  get hair() {
+    return this.#_hair
+  }
+
+  set hair(value) {
+    this.#_hair = value
+  }
+
+  get hat() {
+    return this.#_hat
+  }
+
+  set hat(value) {
+    this.#_hat = value
+  }
+
+  get headPose() {
+    return this.#_headPose
+  }
+
+  set headPose(value) {
+    this.#_headPose = value
+  }
+
+  get mask() {
+    return this.#_mask
+  }
+
+  set mask(value) {
+    this.#_mask = value
+  }
+
+  get moustache() {
+    return this.#_moustache
+  }
+
+  set moustache(value) {
+    this.#_moustache = value
+  }
+
+  get mouth() {
+    return this.#_mouth
+  }
+
+  set mouth(value) {
+    this.#_mouth = value
+  }
+
+  get nose() {
+    return this.#_nose
+  }
+
+  set nose(value) {
+    this.#_nose = value
+  }
+
+  get shape() {
+    return this.#_shape
+  }
+
+  set shape(value) {
+    this.#_shape = value
+  }
+
+  get skin() {
+    return this.#_skin
+  }
+
+  set skin(value) {
+    this.#_skin = value
+  }
+
+  get smile() {
+    return this.#_smile
+  }
+
+  set smile(value) {
+    this.#_smile = value
+  }
+
+  get requestId() {
+    return this.#_requestId
+  }
+
+  set requestId(value) {
+    this.#_requestId = value
+  }
+}
+
+export {FaceDetectResult}
